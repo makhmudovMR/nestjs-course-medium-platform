9b17d20223fc9c1b6b4fdc045f3be000769c726b
Magomed Makhmudov <mmriadovich@gmail.com>
2022-07-12 16:25:54 +0300

    connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagEntity } from '@app/tag/tag.entity';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'mediumclone',
+      password: '123',
+      database: 'mediumclone',
+      entities: [TagEntity],
+      synchronize: true,
+    }),
+    TagModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
